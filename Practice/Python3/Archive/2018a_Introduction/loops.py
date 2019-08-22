e4c58f7bfeb9b04421fd5905f8e86a44b5630b59
Glenn Lopez <glennlopez@gmail.com>
2019-08-22 14:24:55 -0500

    folder restructure

---
+++ Practice/Python3/Archive/2018a_Introduction/loops.py
@@ -0,0 +1,10 @@
+#https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=false
+
+if __name__ == '__main__':
+    n = int(input())
+
+x = 0
+y = 0
+for i in range(n):
+    print (x**2)
+    x += 1
